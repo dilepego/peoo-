162fd1b63a7d8d80834b83976a2314f38a73a75e
Diego Luiz <124363859+dilepego@users.noreply.github.com>
2023-05-08 10:04:35 -0300

    Add files via upload

---
+++ lista07-04.py
@@ -0,0 +1,19 @@
+def maior(x,y):
+  if x > y:
+    
+    return True
+  
+  else:
+
+    return False
+
+x = float(input('dê um valor pra x'))
+y = float(input('dê um valor pra y'))
+
+if maior(x,y):
+  print('x é o maior numero')
+
+else:
+  print('y é o maior numero')
+    
+
